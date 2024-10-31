2e5a9d7492eff2099f8894e24968425e9b99cb2d
Ruslan Tejerina <r.tejerinazap@outlook.es>
2024-10-31 20:42:47 +0100

    create PasswordEncoder and AuthenticationManager

--- src/main/java/com/springboot/backend/ruslan/usersapp/users_backend/auth/SpringSecurityConfig.java
+++ src/main/java/com/springboot/backend/ruslan/usersapp/users_backend/auth/SpringSecurityConfig.java
@@ -1,16 +1,51 @@
 package com.springboot.backend.ruslan.usersapp.users_backend.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.springboot.backend.ruslan.usersapp.users_backend.auth.filter.JWTAuthenticationFilter;
 
 
 @Configuration
 public class SpringSecurityConfig {
     
+    @Autowired
+    private AuthenticationConfiguration authConfiguration;
+
+    /**
+     * Este metodo nos permite poder obtener el componente AuthenticationManager que es un componente central en Spring Security
+     * que se encarga de autenticar las credenciales del usuario.
+     * Este bean se obtiene llamando al método getAuthenticationManager() de authConfiguration, 
+     * que es una instancia de una clase de configuración de seguridad. 
+     * @return nos devuelve el AuthenticationManager de la configuración de seguridad.
+     * @throws Exception si hay un error en la configuración de seguridad.
+     * Al ser un componente @Bean, Spring lo gestiona y lo inyecta en otras clases.
+     * Además de obtener el AuthenticationManager, también se configura el filtro de seguridad mas adelante.
+     */
+    @Bean 
+    AuthenticationManager authenticationManager() throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    /**
+     * Método que nos permite encriptar la contraseña de los usuarios.
+     * Creamos un objeto de la clase BCryptPasswordEncoder la devolvemos y con @Bean le decimos a Spring que lo gestione en el contenedor de beans.
+     * @return devuelve un objeto de la clase BCryptPasswordEncoder.
+     */
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Se crea un objeto de la clase BCryptPasswordEncoder con la configuración por defecto.
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -22,6 +57,7 @@ public class SpringSecurityConfig {
             .requestMatchers(HttpMethod.PUT, "/api/users/{id}").hasRole("ADMIN")
             .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasRole("ADMIN")
             .anyRequest().authenticated())
+        .addFilter(new JWTAuthenticationFilter(authenticationManager()))
         .csrf(config -> config.disable())
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
---
+++ src/main/java/com/springboot/backend/ruslan/usersapp/users_backend/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,139 @@
+package com.springboot.backend.ruslan.usersapp.users_backend.auth.filter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.backend.ruslan.usersapp.users_backend.entities.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import static com.springboot.backend.ruslan.usersapp.users_backend.auth.TokenJWTConfig.*;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+      
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        String username = null;
+        String password = null;
+      
+
+        try {
+            /**
+             * Se crea un objeto User a partir de los datos del usuario que se envían en la petición.
+             * El método readValue de ObjectMapper se utiliza para deserializar (convertir) datos JSON en un objeto Java.
+             * request.getInputStream() proporciona el flujo de entrada que contiene los datos JSON.
+             * User.class es la clase de destino a la que se desea convertir los datos JSON.
+             */
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (StreamReadException e) {
+         
+            e.printStackTrace();
+        } catch (DatabindException e) {
+           
+            e.printStackTrace();
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+
+       UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+       
+        return this.authenticationManager.authenticate(authenticationToken);
+    }
+
+    /**
+     * Este metodo genera un token de autenticación si la autenticación es exitosa.
+     */
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        
+        String username = user.getUsername();
+
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+        /**
+         * Creamos un objeto Claims para almacenar información adicional en el token.
+         * En este caso, almacenamos los roles del usuario y el nombre de usuario.
+         */
+        Claims claims = Jwts
+                .claims()
+                .add("authorities", new ObjectMapper().writeValueAsString(roles))
+                .add("username", username)
+                .build();
+
+        String jwt = Jwts.builder()
+            .subject(username)
+            .claims(claims)  //Añade la información adicional al token de autenticación como los roles y el nombre de usuario.
+            .signWith(SECRET_KEY) //Firma el token con la clave secreta.
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + 3600000)) //1 hora expiración del token.
+            .compact(); //Genera el token de autenticación.
+
+        //Pasamos en la cabecera de la respuesta el token de autenticación.
+        /** El esquema "Bearer" se especifica en el encabezado Authorization de una solicitud HTTP 
+         * para indicar que el cliente está proporcionando un token de acceso que debe ser utilizado 
+         * para autenticar la solicitud.
+         */
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + jwt);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", jwt);
+        body.put("username", username);
+        body.put("message", String.format("%s Autenticación exitosa", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body)); //Convierte a un String con estructura JSON.
+        response.setContentType(CONTENT_TYPE); //Tipo de contenido JSON.
+        response.setStatus(200); //Código de estado HTTP 200 OK.
+    }
+    /**
+     * Este metodo se ejecuta si la autenticación falla manda el error HTTP 401 de no autorizado.
+     * Estos errores se comprueban con las validaciones en el Postman.
+     */
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        
+        Map<String, String> body = new HashMap<>();
+
+        body.put("message", "Error de autenticación: " + failed.getMessage());
+        body.put("error", failed.getMessage());
+        
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(401);
+    }
+
+}
